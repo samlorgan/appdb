a9bcfa6bf335fa32c2436b55947b164854f601a0
samuelorgan <sam.l.organ@gmail.com>
2023-11-25 16:52:24 +0000

    moremain

--- dbapp/db/database.go
+++ dbapp/db/database.go
@@ -1,29 +1,30 @@
 package db
 
 import (
+	"context"
 	"dbapp/ent"
 	"fmt"
 	"log"
+	"time"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type IDatabase interface {
-	// CreateCountry(code string, name string, currency string, sanctioned bool, armySize int) (*ent.Country, error)
-	// SaveCountry(country *ent.Country) (*ent.Country, error)
-	// UpdateCountry(country *ent.Country) (*ent.Country, error)
-	// DeleteCountry(country *ent.Country) error
-	// CreateCustomer(firstName string, lastName string, email string, phone string, address string, favouriteColour *string, country ent.Country) (*ent.Customer, error)
-	// SaveCustomer(customer *ent.Customer) (*ent.Customer, error)
-	// UpdateCustomer(customer *ent.Customer) (*ent.Customer, error)
-	// DeleteCustomer(customer *ent.Customer) error
-	// CreateProductLine(productCode int, itemName string, itemDescription string, itemPrice float64, itemKg float64, manufacturer ent.Country, assembler ent.Country, bannedCountries []*ent.Country) (*ent.ProductLine, error)
-	// SaveProductLine(productLine ent.ProductLine) (*ent.ProductLine, error)
-	// UpdateProductLine(productLine ent.ProductLine) (*ent.ProductLine, error)
-	// DeleteProductLine(productLine *ent.ProductLine) error
-	// GetAllCountries() []*ent.Country
-	// GetAllProductLines() []*ent.ProductLine
-	// GetAllCustomers() []*ent.Customer
-	// GetAllOrders() []*ent.Order
-	// GetAllOrderItems() []*ent.OrderItems
+	CreateApplicationCategory(name string, displayOrder int) (*ent.ApplicationCategory, error)
+	GetAllApplicationCategories() []*ent.ApplicationCategory
+	CreateApplication(name string, description string, altText string, uri string, iconURI string, isFavourite bool, validFrom time.Time, validTo time.Time, applicationCategory *ent.ApplicationCategory) (*ent.Application, error)
+	GetAllApplications() []*ent.Application
+	CreateADGroup(name string) (*ent.ADGroup, error)
+	GetAllADGroups() []*ent.ADGroup
+	CreateCommunity(whamSiteID int, whamTitle string, whanDescription string, whamCommunityURL string, whamCreated time.Time, whamUpdated time.Time, featuredFrom time.Time, featuredTo time.Time, adGroups []*ent.ADGroup, communityCategories []*ent.CommunityCategory) (*ent.Community, error)
+	GetAllCommunities() []*ent.Community
+	CreateCommunityCategory(name string, displayOrder int) (*ent.CommunityCategory, error)
+	GetAllCommunityCategories() []*ent.CommunityCategory
+	CreatePartner(whamSiteID int, whamTitle string, whamDescription string, KeycloakOrganisation string, whamPartnerURL string, whamCreated time.Time, whamUpdated time.Time, applications []*ent.Application) (*ent.Partner, error)
+	GetAllPartners() []*ent.Partner
+	CreatePartnerPageLinkFragment(linkText string, whamPartnerURLSuffix string, displayOrder int) (*ent.PartnerPageLinkFragment, error)
+	GetAllPartnerPageLinkFragments() []*ent.PartnerPageLinkFragment
 }
 
 type Database struct {
@@ -43,6 +44,173 @@ func Init() *Database {
 	}
 	return &Database{client: client}
 
+}
+func (d *Database) CreateApplicationCategory(name string, displayOrder int) (*ent.ApplicationCategory, error) {
+	createdApplicationCategory, err := d.client.ApplicationCategory.Create().
+		SetName(name).SetDisplayOrder(displayOrder).
+		Save(context.Background())
+	if err != nil {
+		fmt.Printf("failed creating application category: %v\n", err)
+		return nil, fmt.Errorf("failed creating application category: %v", err)
+	}
+	return createdApplicationCategory, nil
+
+}
+func (d *Database) GetAllApplicationCategories() []*ent.ApplicationCategory {
+	applicationCategories, err := d.client.ApplicationCategory.Query().All(context.Background())
+	if err != nil {
+		fmt.Printf("failed getting all application categories: %v\n", err)
+	}
+	fmt.Printf("Application Categories: %+v\n", applicationCategories)
+	return applicationCategories
+}
+func (d *Database) CreateApplication(name string, description string, altText string, uri string, iconURI string,
+	isFavourite bool, validFrom time.Time, validTo time.Time, applicationCategory *ent.ApplicationCategory) (*ent.Application, error) {
+	application, err := d.client.Application.Create().
+		SetName(name).
+		SetDescription(description).
+		SetAltText(altText).
+		SetURI(uri).
+		SetIconURI(iconURI).
+		SetIsFavourite(isFavourite).
+		SetValidFrom(validFrom).
+		SetValidTo(validTo).
+		SetApplicationCategory(applicationCategory).
+		Save(context.Background())
+	if err != nil {
+		fmt.Printf("failed creating application: %v\n", err)
+		return nil, fmt.Errorf("failed creating application category: %v", err)
+	}
+	fmt.Printf("Application: %+v\n", application)
+	return application, nil
+}
+func (d *Database) GetAllApplications() []*ent.Application {
+	applications, err := d.client.Application.Query().All(context.Background())
+	if err != nil {
+		fmt.Printf("failed getting all applications: %v\n", err)
+	}
+	fmt.Printf("Applications: %+v\n", applications)
+	return applications
+}
+func (d *Database) CreateADGroup(name string) (*ent.ADGroup, error) {
+	adGroup, err := d.client.ADGroup.Create().
+		SetName(name).
+		Save(context.Background())
+	if err != nil {
+		fmt.Printf("failed creating ad group: %v\n", err)
+		return nil, fmt.Errorf("failed creating application category: %v", err)
+	}
+	fmt.Printf("ADGroup: %+v\n", adGroup)
+	return adGroup, nil
+}
+func (d *Database) GetAllADGroups() []*ent.ADGroup {
+	adGroups, err := d.client.ADGroup.Query().All(context.Background())
+	if err != nil {
+		fmt.Printf("failed getting all ad groups: %v\n", err)
+	}
+	fmt.Printf("ADGroups: %+v\n", adGroups)
+	return adGroups
+}
+func (d *Database) CreateCommunity(whamSiteID int, whamTitle string, whamDescription string,
+	whamCommunityURL string, whamCreated time.Time, whamUpdated time.Time, featuredFrom time.Time,
+	featuredTo time.Time, adGroups []*ent.ADGroup, communityCategories []*ent.CommunityCategory) (*ent.Community, error) {
+	community, err := d.client.Community.Create().
+		SetWhamSiteID(whamSiteID).
+		SetWhamTitle(whamTitle).
+		SetWhanDescription(whamDescription).
+		SetWhamCommunityURL("Test Community").
+		SetWhamCreated(time.Now()).
+		SetWhamUpdated(time.Now()).
+		SetFeaturedFrom(time.Now()).
+		SetFeaturedTo(time.Now()).
+		AddAdgroup(adGroups...).
+		AddCommunityCategory(communityCategories...).
+		Save(context.Background())
+	if err != nil {
+		fmt.Printf("failed creating community: %v\n", err)
+		return nil, fmt.Errorf("failed creating community: %v", err)
+	}
+	fmt.Printf("Community: %+v\n", community)
+	return community, nil
+}
+func (d *Database) GetAllCommunities() []*ent.Community {
+	communities, err := d.client.Community.Query().All(context.Background())
+	if err != nil {
+		fmt.Printf("failed getting all communities: %v\n", err)
+	}
+	fmt.Printf("Communities: %+v\n", communities)
+	return communities
+}
+func (d *Database) CreatePartner(WhamSiteID int, WhamTitle string, WhamDescription string, KeycloakOrganisation string,
+	WhamPartnerURL string, WhamCreated time.Time, WhamUpdated time.Time, applications []*ent.Application) (*ent.Partner, error) {
+	partner, err := d.client.Partner.Create().
+		SetWhamSiteID(WhamSiteID).
+		SetWhamTitle(WhamTitle).
+		SetWhamDescription(WhamDescription).
+		SetKeycloakOrganisation(KeycloakOrganisation).
+		SetWhamPartnerURL(WhamPartnerURL).
+		SetWhamCreated(WhamCreated).
+		SetWhamUpdated(WhamUpdated).
+		AddApplication(applications...).
+		Save(context.Background())
+	if err != nil {
+		fmt.Printf("failed creating partner: %v\n", err)
+		return nil, fmt.Errorf("failed creating partner: %v", err)
+	}
+	fmt.Printf("Partner: %+v\n", partner)
+	return partner, nil
+}
+func (d *Database) GetAllPartners() []*ent.Partner {
+	partners, err := d.client.Partner.Query().All(context.Background())
+	if err != nil {
+		fmt.Printf("failed getting all partners: %v\n", err)
+	}
+	fmt.Printf("Partners: %+v\n", partners)
+	return partners
+}
+
+func (d *Database) CreatePartnerPageLinkFragment(linkText string, whamPartnerURLSuffix string, displayOrder int) (*ent.PartnerPageLinkFragment, error) {
+	partnerPageLinkFragment, err := d.client.PartnerPageLinkFragment.Create().
+		SetLinkText(linkText).
+		SetWhamPartnerURLSuffix(whamPartnerURLSuffix).
+		SetDisplayOrder(displayOrder).
+		Save(context.Background())
+	if err != nil {
+		fmt.Printf("failed creating partner page link fragment: %v\n", err)
+		return nil, fmt.Errorf("failed creating partner page link fragment: %v", err)
+	}
+	fmt.Printf("Partner Page Link Fragment: %+v\n", partnerPageLinkFragment)
+	return partnerPageLinkFragment, nil
+}
+
+func (d *Database) GetAllPartnerPageLinkFragments() []*ent.PartnerPageLinkFragment {
+	partnerPageLinkFragments, err := d.client.PartnerPageLinkFragment.Query().All(context.Background())
+	if err != nil {
+		fmt.Printf("failed getting all partner page link fragments: %v\n", err)
+	}
+	fmt.Printf("Partner Page Link Fragments: %+v\n", partnerPageLinkFragments)
+	return partnerPageLinkFragments
+}
+func (d *Database) CreateCommunityCategory(name string, displayOrder int) (*ent.CommunityCategory, error) {
+	communityCategory, err := d.client.CommunityCategory.Create().
+		SetName(name).
+		SetDisplayOrder(displayOrder).
+		Save(context.Background())
+	if err != nil {
+		fmt.Printf("failed creating community category: %v\n", err)
+		return nil, fmt.Errorf("failed creating community category: %v", err)
+	}
+	fmt.Printf("Community Category: %+v\n", communityCategory)
+	return communityCategory, nil
+}
+
+func (d *Database) GetAllCommunityCategories() []*ent.CommunityCategory {
+	communityCategories, err := d.client.CommunityCategory.Query().All(context.Background())
+	if err != nil {
+		fmt.Printf("failed getting all community categories: %v\n", err)
+	}
+	fmt.Printf("Community Categories: %+v\n", communityCategories)
+	return communityCategories
 }
 
 // func (d *Database) CreateCountry(code string, name string, currency string, sanctioned bool, armySize int) (*ent.Country, error) {
